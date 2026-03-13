747d5d439857d4cec6c8f10ba9fd61bf018cbe3a
AlanBR72 <50175645+AlanBR72@users.noreply.github.com>
2026-03-13 12:10:04 -0300

    Update bot_guilda.py

--- bot_guilda.py
+++ bot_guilda.py
@@ -1,2 +1,2 @@
 r = requests.get("https://www.rucoyonline.com/characters/Alan%20Virtue")
-print(r.text)  # veja se "Last online" aparece mesmo 
+print(r.text)  # veja se "Last online" aparece mesmo kk
